972cfec7ac33c3ea73f2349b4f064c7352abda15
Mehdi <mhdyk3613@gmail.com>
2026-09-22 14:42:56 +0330

    fix missing escapeHtml helper

--- app.js
+++ app.js
@@ -30,6 +30,15 @@
 })();
     "use strict";
 
+    function escapeHtml(value) {
+        return String(value ?? "")
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+
     /* =====================================================
        CONFIG
     ===================================================== */
